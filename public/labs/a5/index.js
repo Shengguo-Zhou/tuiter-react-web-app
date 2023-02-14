996d3103f188a95026fef853c1215d2dba358fdc
Shengguo Zhou <96205839+Shengguo-Zhou@users.noreply.github.com>
2023-02-13 23:42:23 -0800

    a5 add

--- public/labs/a5/index.js
+++ public/labs/a5/index.js
@@ -26,7 +26,7 @@ let isString = typeof stringVariable;
 // console.log(isString);
 let isBoolean = typeof booleanVariable
 // console.log(isBoolean);
-console.log('Boolean Variables');
+// console.log('Boolean Variables');
 let true1 = true;
 let false1 = false;
 let false2 = true1 && false1;
@@ -136,23 +136,18 @@ const oddNumbers = numberArray1
 // console.log(oddNumbers);
 const five = 2 + 3;
 const result1 = "2 + 3 = " + five;
-console.log(result1);
+// console.log(result1);
 
 const result2 = `2 + 3 = ${2 + 3}`;
-console.log(result2);
+// console.log(result2);
 
 const username = "alice";
 const greeting1 = `Welcome home ${username}`;
-console.log(greeting1);
+// console.log(greeting1);
 
 loggedIn = false;
 const greeting2 = `Logged in: ${loggedIn ? "Yes" : "No"}`;
-console.log(greeting2);
-
-
-
-
-
+// console.log(greeting2);
 
 
 
