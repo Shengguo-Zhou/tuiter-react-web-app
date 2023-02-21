a54ff12e9225baa52a839a53e51f002a2762d21b
Shengguo Zhou <96205839+Shengguo-Zhou@users.noreply.github.com>
2023-02-21 14:29:09 -0800

    add all

---
+++ src/tuiter/NavigationSidebar/NavigationSidebarItem.js
@@ -0,0 +1,39 @@
+export default [
+  {class: 'fab fa-twitter',
+    name:'',
+    href:'../explore/index.html'
+  },
+  {class: 'fas fa-house-user override-lg-color',
+    name:'Home',
+    href:'../home/index.html'
+  },
+  {class: 'fas fa-hashtag',
+    name:'Explore',
+    href:'../explore/index.html'
+  },
+  {class: 'fas fa-bell',
+    name:'Notification',
+    href:'../tuiter'
+  },
+  {class: 'fas fa-envelope',
+    name:'Messages',
+    href:'../tuiter'
+  },
+  {class: 'fas fa-bookmark',
+    name:'Bookmarks',
+    href:'../tuiter'
+  },
+  {class: 'fas fa-list',
+    name:'Articles',
+    href:'../tuiter'
+  },
+  {class: 'fas fa-user',
+    name:'Profile',
+    href:'../tuiter'
+  },
+  {class: 'fas fa-comment-dots',
+    name:'More',
+    href:'../tuiter'
+  },
+
+];
--- src/tuiter/NavigationSidebar/index.js
+++ src/tuiter/NavigationSidebar/index.js
@@ -1,29 +1,42 @@
 import React from "react";
+import items from "./NavigationSidebarItem.js"
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
 const NavigationSidebar = (
-    { active = 'explore'}
-  ) => {
-  return (
-      <div className="list-group">
-        <a className="list-group-item">Tuiter</a>
-        <a className={`list-group-item ${active === 'home'?'active':''}`}>
-          Home</a>
-        <a className={`list-group-item ${active === 'explore'?'active':''}`}>
-          Explore</a>
-        <a className={`list-group-item ${active === 'notifications'?'active':''}`}>
-          Notifications</a>
-        <a className={`list-group-item ${active === 'messages'?'active':''}`}>
-          Messages</a>
-        <a className={`list-group-item ${active === 'bookmarks'?'active':''}`}>
-          Bookmarks</a>
-        <a className={`list-group-item ${active === 'lists'?'active':''}`}>
-          Lists</a>
-        <a className={`list-group-item ${active === 'profile'?'active':''}`}>
-          Profile</a>
-        <a className={`list-group-item ${active === 'more'?'active':''}`}>
-          More</a>
-      </div>
+    { active = 'Explore'}) => {
+      return (
+      <>
+        <div className="list-group">
+          <ul>
+          {
+          items.map(item => {
+            return (<>
+              <a className={`list-group-item override-lgi list-group-item-action
+              ${active === item.name ? "active" : ""}`}
+              href={`${item.href}`}>
+                <div className="row">
+                  <div className="col-2">
+                    <i className="bi bi-house"/>
+                  </div>
+                  <div className="col-8">
+                    <span className="d-none d-xl-block d-xxl-block fw-light">{item.name}</span>
+                  </div>
+                </div>
+              </a>
+          </>);
+          })
+        }
+          </ul>
+        </div>
+
+        <div className="d-grid mt-2">
+          <a href="../NavigationSidebar/tweet.html"
+             className="btn btn-primary btn-block rounded-pill">
+            Tweet</a>
+        </div>
+
+      </>
   );
-};
+}
 
 export default NavigationSidebar;
