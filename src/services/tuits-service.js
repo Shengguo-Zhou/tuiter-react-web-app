c108c721e9493920721482dd063bfa4b57cf2916
Shengguo Zhou <96205839+Shengguo-Zhou@users.noreply.github.com>
2023-04-02 17:21:24 -0700

    a9 all

--- src/services/tuits-service.js
+++ src/services/tuits-service.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 // assignment 9
-const TUITS_API = 'http://localhost:4000/api/tuits';
-// const TUITS_API = 'https://tuiter-node-server-app-0322.onrender.com/api/tuits';
+// const TUITS_API = 'http://localhost:4000/api/tuits';
+const TUITS_API = 'https://tuiter-node-server-app-0402.onrender.com/api/tuits';
 // const API_BASE = process.env.REACT_APP_API_BASE;
 // const TUITS_API = `${API_BASE}/tuits`;
 
